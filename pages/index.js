a60c669deb956d473982bb6068519f9d606507fc
prabal-github <prabal.chow09009.pc@gmail.com>
2022-05-24 21:12:23 +0530

    button large

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-      <div className="h-screen w-screen flex justify-center items-center bg-red-500">
-        {/* <div className='h-3/6 w-3/6 bg-yellow-200 m-24 justify-end	'>          */}
-        <div className='h-3/6 w-3/6 flex justify-center items-center align-middle content-center bg-yellow-200'>         
-          <Link href='/student'><button type="button" className="btn btn-primary m-24">Student</button></Link>
-          <Link href='/login'><button type="button" className="btn btn-secondary m-24">Admin</button></Link>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className='h-3/6 w-3/6 flex justify-center items-center'>         
+          <Link href='/student'><button type="button" className="btn btn-primary btn-lg">Student</button></Link>
+          <Link href='/login'><button type="button" className="btn btn-secondary btn-lg">Admin</button></Link>
         </div>
       </div>
   )
